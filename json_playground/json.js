0511d6fb11afe4e364bb19dc7fe0df3e54e682de
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-09-03 13:51:21 +0700

    working with json

---
+++ json_playground/json.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some Title',
+    body: 'Some body'
+}
+
+var originalNoteString = JSON.stringify(originalNote);
+//console.log(originalNoteString)
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note)
+console.log(note.title);
