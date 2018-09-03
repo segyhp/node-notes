2c55e645941a430368e75bdd71198341010e6108
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-09-03 13:51:40 +0700

    adding and saving notes

--- app.js
+++ app.js
@@ -3,40 +3,24 @@ const fs = require('fs');
 const notes = require('./notes.js');
 const yargs = require('yargs');
 const _ = require('lodash');
-
-
-
 var argv = yargs.argv;
 //var command = process.argv[2];
 var command = argv._[0];
 //console.log("length process " + process.argv.length)
-
 //console.log(process.argv)
-
 console.log('Process: ', process.argv)
-console.log("Command yargs ",  argv)
-
+console.log("Command yargs ", argv)
 if (command === 'add') {
     notes.addNote(argv.title, argv.body)
-    
 } else if (command === 'list') {
-        
     notes.getAll();
-    
 } else if (command === 'read') {
-    
     notes.readNote(argv.title)
 } else if (command === 'delete') {
-    
-    
-        notes.deleteNote(argv.title)
-    
+    notes.deleteNote(argv.title)
 } else {
     console.log("Command not recognized")
 }
-
-
-
 //var res = notes.addNote();
 //
 //var num = notes.addNumber(4,-15);
@@ -60,6 +44,33 @@ if (command === 'add') {
 //var filteredArray = _.uniq(['Segy','Hendro',2,5,1,1,3])
 //console.log(filteredArray);
 //const os = require('os');
-
-
-
+// Sum of array
+//var a = [1,2,3,4,5];
+//
+//var result = 0;
+//
+//for(let i = 0; i < a.length; i++){
+//    console.log("angka " + a[i])
+//    result += a[i]
+//    
+//    
+//}
+//
+//console.log("result " + result)
+//plus-minus problem
+//var arr =[1, 2, 3, -1, -2, -3, 0, 0]
+//let positive = 0
+// let negative = 0
+// let zero = 0
+// 
+// arr.forEach((element) => {
+//     if(element > 0) positive++
+//     else if(element < 0) negative++
+//     else zero ++
+// })
+// positive = positive/arr.length
+// negative = negative/arr.length
+// zero = zero/arr.length
+//    
+//console.log( positive.toFixed(6) + '\n' + negative.toFixed(6) + '\n' +zero.toFixed(6))
+//
--- notes.js
+++ notes.js
@@ -1,36 +1,52 @@
 console.log('Starting notes.js');
 
+const fs = require('fs')
+
+var addNote = (title, body) => {
+    //    console.log("Adding notes ", title, body)
+    var notes = [];
+    var note = {
+        title,
+        body
+    }
+
+    try {
+
+        var notesString = fs.readFileSync('notes-data.json')
+        notes = JSON.parse(notesString);
+
+    } catch (e) {}
 
 
-var addNote = (title, body) =>{
-    console.log("Adding notes ", title, body)
+    var duplicateNotes = notes.filter((note) => note.title === title)
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note)
+
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+    }
+
+
 }
-
 var getAll = () => {
     console.log("Get all the notes")
 }
-
 var readNote = (title) => {
-    console.log("Read the title : " + title )
+    console.log("Read the title : " + title) 
 }
-
 var deleteNote = (title) => {
     console.log("Delete the note : " + title)
 }
-
 module.exports = {
     addNote,
     getAll,
     readNote,
     deleteNote
 };
-
 //module.exports.addNote =  function () {
 //    console.log("add note.");
 //    return "New Note";
 //}
-
-
 //module.exports.addNumber = (a,b) => {
 //    console.log('a plus b');
 //    return a + b;
