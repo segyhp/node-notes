52c058899840eabf7be6f7c78f953d2dae504eef
segyhp <segyhendropratama@gmail.com>
2020-03-25 23:37:53 +0700

    feat: 🎸 try validator

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const validator = require('validator');
+
 const fs = require("fs");
 const notes = require('./notes.js');
-fs.writeFileSync('notes.txt', "This file is created by with Node JS.");
-fs.appendFileSync('notes.txt', '\nThis is the additional data for notes.');
+//fs.writeFileSync('notes.txt', "This file is created by with Node JS.");
+//fs.appendFileSync('notes.txt', '\nThis is the additional data for notes.');
 
 const msg = notes();
 
-console.log(msg);
+console.log();
+
+console.log(validator.isEmail('email@email.com'));
+console.log(validator.isURL('hehe'));
