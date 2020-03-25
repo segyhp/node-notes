159b38d07fb1c364f7ecceff054f4781ae0abd4c
segyhp <segyhendropratama@gmail.com>
2020-03-25 21:06:48 +0700

    feat: 🎸 import custom file to main file

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const fs = require("fs");
+const notes = require('./notes.js');
 fs.writeFileSync('notes.txt', "This file is created by with Node JS.");
 fs.appendFileSync('notes.txt', '\nThis is the additional data for notes.');
+
+const msg = notes();
+
+console.log(msg);
