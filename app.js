7c721891832dfc630cc97f1703bad75467632bde
segyhp <segyhendropratama@gmail.com>
2020-03-30 12:54:52 +0700

    feat: 🎸 use process argv

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const validator = require('validator');
 const chalk = require("chalk");
-
-const fs = require("fs");
 const notes = require('./notes.js');
-//fs.writeFileSync('notes.txt', "This file is created by with Node JS.");
-//fs.appendFileSync('notes.txt', '\nThis is the additional data for notes.');
 
-const msg = notes();
+const command = process.argv[2];
 
-console.log(msg);
+console.info(process.argv);
 
-console.log(chalk.green.bold.bgBlueBright('Success!..'));
-
-console.log(validator.isEmail('email@email.com'));
-console.log(validator.isURL('hehe'));
+if (command === 'add') {
+    console.log("Adding note");
+} else if (command === 'delete') {
+    console.log('deleting note..');
+}
